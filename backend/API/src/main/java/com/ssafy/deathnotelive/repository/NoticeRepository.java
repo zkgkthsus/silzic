53f60c3ad3e1d202388908eb1353776bb87a34a6
zkgkthsus <zkgkthsus@naver.com>
2022-04-14 22:47:13 +0900

    remove others

--- backend/API/src/main/java/com/ssafy/deathnotelive/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.deathnotelive.repository;
-
-import com.ssafy.deathnotelive.entity.Notice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
